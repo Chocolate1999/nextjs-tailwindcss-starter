df6241ad6e388f07829fdc7d95f6f190ee5d868d
Chocolate1999 <2816778271@qq.com>
2022-10-01 21:04:45 +0800

    feat: remove recoil

--- next.config.js
+++ next.config.js
@@ -3,22 +3,7 @@ module.exports = {
   eslint: {
     dirs: ['src'],
   },
-
-  // Uncoment to add domain whitelist
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: [],
   },
 };
-
-const intercept = require('intercept-stdout');
-
-// safely ignore recoil stdout warning messages
-function interceptStdout(text) {
-  if (text.includes('Duplicate atom key')) {
-    return '';
-  }
-  return text;
-}
-
-// Intercept in dev and prod
-intercept(interceptStdout);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app';
 import Router from 'next/router';
 import { ThemeProvider } from 'next-themes';
 import nProgress from 'nprogress';
-import { RecoilRoot } from 'recoil';
 
 import '@/styles/globals.css';
 import '@/styles/nprogress.css';
@@ -17,11 +16,9 @@ Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ThemeProvider attribute='class' enableSystem={true}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider attribute='class' enableSystem={true}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
